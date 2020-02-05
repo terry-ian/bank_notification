8429e52cd7b9c22453a4abd8a29ab34c9745466c
terry-ian <39936938+terry-ian@users.noreply.github.com>
2020-02-05 14:13:36 +0800

    Add files via upload

--- bank_file/bank_mysql_function.py
+++ bank_file/bank_mysql_function.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env python
 # coding: utf-8
 import pymysql
+import pandas as pd
+
 def sql_webcrawler(url,postdate,bank,title,content):
-    db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
-    cursor = db.cursor() # 创建一个游标对象
-    # 插入语句
-    sql = "INSERT INTO webcrawler_bank(url,postdate,bank,title,content) "  "VALUES ('%s','%s','%s','%s','%s')" % (url,postdate,bank,title,content)
-    try:
-        cursor.execute(sql)  # 执行 SQL 插入语句
-    except:
-        db.rollback()  # 如果发生错误则回滚
-    db.commit() # 提交到数据库执行
-    cursor.close() #关闭游标
-    db.close() #关闭连接
-    
+	db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
+	cursor = db.cursor() # 创建一个游标对象
+	# 插入语句
+	sql = "INSERT INTO webcrawler_bank(url,postdate,bank,title,content) "  "VALUES ('%s','%s','%s','%s','%s')" % (url,postdate,bank,title,content)
+	try:
+		cursor.execute(sql)  # 执行 SQL 插入语句
+	except:
+		db.rollback()  # 如果发生错误则回滚
+	db.commit() # 提交到数据库执行
+	cursor.close() #关闭游标
+	db.close() #关闭连接
+	
 def sql_notification(bank,title,notes,url,status):
-    db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
-    cursor = db.cursor() # 创建一个游标对象
-    # 插入语句
-    sql = "INSERT INTO notification_bank(bank,title,notes,url,status) "  "VALUES ('%s','%s','%s','%s','%s')" % (bank,title,notes,url,status) 
-    try:
-        cursor.execute(sql)  # 执行 SQL 插入语句
-    except:
-        db.rollback()  # 如果发生错误则回滚
-    db.commit() # 提交到数据库执行
-    cursor.close() #关闭游标
-    db.close() #关闭连接
+	db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
+	cursor = db.cursor() # 创建一个游标对象
+	# 插入语句
+	sql = "INSERT INTO notification_bank(bank,title,notes,url,status) "  "VALUES ('%s','%s','%s','%s','%s')" % (bank,title,notes,url,status) 
+	try:
+		cursor.execute(sql)  # 执行 SQL 插入语句
+	except:
+		db.rollback()  # 如果发生错误则回滚
+	db.commit() # 提交到数据库执行
+	cursor.close() #关闭游标
+	db.close() #关闭连接
 
 def select_sql(sqlcontent):
-    db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
-    sql_select = sqlcontent
-    df = pd.read_sql(sql_select, con=db)
-    return(df)
+	db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
+	sql_select = sqlcontent
+	df = pd.read_sql(sql_select, con=db)
+	return(df)
+
 
