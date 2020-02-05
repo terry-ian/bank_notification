3d3b193836f355774e731d5b2667c650ee6dfc3a
terry-ian <39936938+terry-ian@users.noreply.github.com>
2020-02-05 10:59:56 +0800

    mysql_function
    帳號 密碼 從這邊更改

---
+++ bank_file/bank_mysql_function.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+def sql_webcrawler(url,postdate,bank,title,content):
+    db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
+    cursor = db.cursor() # 创建一个游标对象
+    # 插入语句
+    sql = "INSERT INTO webcrawler_bank(url,postdate,bank,title,content) "  "VALUES ('%s','%s','%s','%s','%s')" % (url,postdate,bank,title,content)
+    try:
+        cursor.execute(sql)  # 执行 SQL 插入语句
+    except:
+        db.rollback()  # 如果发生错误则回滚
+    db.commit() # 提交到数据库执行
+    cursor.close() #关闭游标
+    db.close() #关闭连接
+    
+def sql_notification(bank,notes,url,status):
+    db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
+    cursor = db.cursor() # 创建一个游标对象
+    # 插入语句
+    sql = "INSERT INTO notification_bank(bank,notes,url,status) "  "VALUES ('%s','%s','%s','%s')" % (bank,notes,url,status)
+    try:
+        cursor.execute(sql)  # 执行 SQL 插入语句
+    except:
+        db.rollback()  # 如果发生错误则回滚
+    db.commit() # 提交到数据库执行
+    cursor.close() #关闭游标
+    db.close() #关闭连接
+
+def select_sql(sqlcontent):
+    db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
+    sql_select = sqlcontent
+    df = pd.read_sql(sql_select, con=db)
+    return(df)
+
