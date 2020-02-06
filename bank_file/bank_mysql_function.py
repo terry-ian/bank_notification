43d6ef107a0534c8fc0b56faa42ea047e15d20ea
terry-ian <39936938+terry-ian@users.noreply.github.com>
2020-02-06 10:54:15 +0800

    Add files via upload

--- bank_file/bank_mysql_function.py
+++ bank_file/bank_mysql_function.py
@@ -2,7 +2,9 @@
 # coding: utf-8
 import pymysql
 import pandas as pd
+import re
 
+#sql写入rowdata表格中语句
 def sql_webcrawler(url,postdate,bank,title,content):
 	db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
 	cursor = db.cursor() # 创建一个游标对象
@@ -15,7 +17,8 @@ def sql_webcrawler(url,postdate,bank,title,content):
 	db.commit() # 提交到数据库执行
 	cursor.close() #关闭游标
 	db.close() #关闭连接
-	
+
+#sql写入警示表格中语句	
 def sql_notification(bank,title,notes,url,status):
 	db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
 	cursor = db.cursor() # 创建一个游标对象
@@ -29,10 +32,43 @@ def sql_notification(bank,title,notes,url,status):
 	cursor.close() #关闭游标
 	db.close() #关闭连接
 
+#sql查询语句	
 def select_sql(sqlcontent):
 	db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
 	sql_select = sqlcontent
 	df = pd.read_sql(sql_select, con=db)
 	return(df)
+	
+#检查有无重复资料写入rowdata表
+def rowdata_db(alldata,noticelen,bankname):
+    #检查数据表中有无重复资料和写入原始资料库中
+    for i in reversed(range(noticelen)):
+        lastdata=select_sql("""select url from webcrawler_bank where bank='"""+bankname+"""'  and url ='"""+alldata[i]["urllink"]+"""' """)
+        if len(lastdata) ==0:
+            sql_webcrawler(alldata[i]["urllink"],alldata[i]["time"],alldata[i]["bank"],alldata[i]["title"],alldata[i]["allcontent"])
 
+#检查有无重复资料写入警示表
+def notification_db(alldata,noticelen,bankname):
+    #文字处理
+    allwarningdata=[]
+    checktitletext=["维护","暂停","升级"] ; checkcontenttext=["将于","约于","定于","计划于","将在","具体时间为","拟于"] #title 关键字 ,content 关键字
+    for check in range(noticelen):
+        if any(re.findall('|'.join(checktitletext), alldata[check]['title'])):
+            warningdata={}
+            if len(re.findall('|'.join(checkcontenttext), alldata[check]['allcontent'])) == 0:
+                noticetext="请查询详细内文"
+            else:
+                stoptime=alldata[check]['allcontent'].find( re.findall('|'.join(checkcontenttext), alldata[check]['allcontent'])[0] )
+                noticetext=alldata[check]['allcontent'][stoptime:stoptime+50].split("。" )[0] if len(alldata[check]['allcontent'][stoptime:stoptime+50].split("。" )[0]) < len(alldata[check]['allcontent'][stoptime:stoptime+50].split("，" )[0]) else alldata[check]['allcontent'][stoptime:stoptime+50].split("，" )[0] 
+            warningdata['bank']=bankname
+            warningdata['title']=alldata[check]['title']
+            warningdata['notes']=noticetext
+            warningdata['url']=alldata[check]['urllink']
+            allwarningdata.append(warningdata)
+    allwarningdatalen=len(allwarningdata)
+    #检查数据表中有无重复资料和写入原始资料库中
+    for i in reversed(range(allwarningdatalen)):
+        lastdatawarning=select_sql("""select url from notification_bank where bank='"""+bankname+"""' and url ='"""+allwarningdata[i]["url"]+"""' """)
+        if len(lastdatawarning) ==0:
+            sql_notification(allwarningdata[i]['bank'],allwarningdata[i]['title'],allwarningdata[i]['notes'],allwarningdata[i]['url'],0)
 
