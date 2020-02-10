0ab1759c019e53723033c67b0cae5d22ad02a1d8
terry-ian <39936938+terry-ian@users.noreply.github.com>
2020-02-10 14:28:57 +0800

    Add files via upload

---
+++ bank_file/bank_parameter.py
@@ -0,0 +1,16 @@
+#手动设置参数文件
+
+#mysql
+db_host='remotemysql.com'
+db_user='giaX9JoXo3'
+db_passwd='VEm7Ky6FIB'
+db_port=3306
+db_database='giaX9JoXo3'
+db_table1='bank_webcrawler'
+db_table2='bank_notification'
+
+#telegram
+tele_chatid='-364811652'
+tele_token='1020859504:AAEb-tLbaBjJvJqBsLCzCsStrgTlZNqXRR8'
+
+
