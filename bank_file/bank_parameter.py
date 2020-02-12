bdfba5d0ddf3495bf8364c7b5fb0e157958a0258
terry-ian <39936938+terry-ian@users.noreply.github.com>
2020-02-12 11:02:27 +0800

    增加 warning message id

--- bank_file/bank_parameter.py
+++ bank_file/bank_parameter.py
@@ -9,7 +9,10 @@ db_database='giaX9JoXo3'
 db_table1='bank_webcrawler'
 db_table2='bank_notification'
 
-#telegram_parameter
+#telegram_parameter group
 tele_chatid='-364811652'
 tele_token='1020859504:AAEb-tLbaBjJvJqBsLCzCsStrgTlZNqXRR8'
 
+#warning message 
+tele_warning_chatid='-364811652'
+tele_warning_token='1020859504:AAEb-tLbaBjJvJqBsLCzCsStrgTlZNqXRR8'
