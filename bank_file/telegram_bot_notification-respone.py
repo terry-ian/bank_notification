ce841e2797e97698896c1a148c1ff72b449c284d
terry-ian <39936938+terry-ian@users.noreply.github.com>
2020-02-12 18:14:41 +0800

    Add files via upload

--- bank_file/telegram_bot_notification-respone.py
+++ bank_file/telegram_bot_notification-respone.py
@@ -15,6 +15,7 @@ import telepot
 from telepot.loop import MessageLoop
 from telepot.namedtuple import ReplyKeyboardMarkup
 from telepot.delegate import pave_event_space, per_chat_id, create_open
+from bank_parameter import *
 #-340019778   -364811652
 
 
@@ -29,23 +30,23 @@ def handle(msg):
         select_sql(msg['text'])
     listtext=['目录','总类','查询']
     if any(re.findall('|'.join(listtext), msg['text'])):
-        bot.sendMessage(chat_id='-364811652',text= "[可查询银行] : '中国工商银行','中国银行','中国农业银行','中国建设银行','中国招商银行','中国光大银行','中国民生银行','交通银行','中信银行','华夏银行','兴业银行','浦发银行','北京银行','天津农商行','内蒙古银行' " )
+        bot.sendMessage(chat_id=tele_chatid ,text= "[可查询银行] : '中国工商银行','中国银行','中国农业银行','中国建设银行','中国招商银行','中国光大银行','中国民生银行','交通银行','中信银行','华夏银行','兴业银行','浦发银行','北京银行','天津农商行','内蒙古银行' " )
 def select_sql(bank):
-    db = pymysql.Connect(host="remotemysql.com",user="giaX9JoXo3",passwd="VEm7Ky6FIB",port=3306,database="giaX9JoXo3",charset = 'utf8')
-    df = pd.read_sql("select * from notification_bank where bank='"+bank+"' order by id desc limit 1", con=db)
+    db = pymysql.Connect(host=db_host,user=db_user,passwd=db_passwd,port=db_port,database=db_database ,charset = 'utf8')
+    df = pd.read_sql("select * from "+db_table2+" where bank='"+bank+"' order by id desc limit 1", con=db)
     value1=df.iloc[0, 3]
     value2=df.iloc[0, 4]
     value3=df.iloc[0, 5]
     value4=df.iloc[0, 6]     #(datetime.datetime.now()).strftime('%Y-%m-%d %H:%M:%S')  #时间加8小时 +datetime.timedelta(hours=8)
-    bot.sendMessage(chat_id='-364811652',text= '[银行名称] : '+value1+ "\n" +'[标题公告] : '+ value2 + "\n" +'[重要讯息] : '+value3+ "\n"+'[讯息网址] : ' +value4)
+    bot.sendMessage(chat_id=tele_chatid,text= '[银行名称] : '+value1+ "\n" +'[标题公告] : '+ value2 + "\n" +'[重要讯息] : '+value3+ "\n"+'[讯息网址] : ' +value4)
 
 # 给出回硬
-bot = telepot.Bot(token='1020859504:AAEb-tLbaBjJvJqBsLCzCsStrgTlZNqXRR8')
+bot = telepot.Bot(token=tele_token)
 MessageLoop(bot,handle).run_as_thread()
 print ('Listening ...')
 # Keep the program running.
-while 1:
-    time.sleep(10)
+#while 1:
+#    time.sleep(10)
 
 
 # In[ ]:
