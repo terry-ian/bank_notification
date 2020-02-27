1e06816639904a98052b387497f2b17cfa949056
terry-ian <39936938+terry-ian@users.noreply.github.com>
2020-02-27 17:25:26 +0800

    Add files via upload

--- sql_file/sql_table.sql
+++ sql_file/sql_table.sql
@@ -1,16 +1,16 @@
-drop database if exists DBbank_crawler;
+drop database if exists bi_notice;
 
-create database DBbank_crawler;
+create database bi_notice;
 
-use DBbank_crawler;
+use bi_notice;
 
 CREATE TABLE IF NOT EXISTS `bank_webcrawler`(
    `id` INT UNSIGNED AUTO_INCREMENT,
    `createtime` TIMESTAMP DEFAULT NOW() ,
-   `url` VARCHAR(256) NOT NULL,
+   `url` VARCHAR(255) NOT NULL,
    `postdate` VARCHAR(40) NOT NULL,
    `bank` VARCHAR(40) NOT NULL,
-   `title` VARCHAR(256) NOT NULL,
+   `title` VARCHAR(255) NOT NULL,
    `content` MEDIUMTEXT  NOT NULL,
    PRIMARY KEY (id),
    UNIQUE KEY unique_url (url)
@@ -20,16 +20,13 @@ CREATE TABLE IF NOT EXISTS `bank_notification`(
    `id` INT UNSIGNED AUTO_INCREMENT ,
    `createtime` TIMESTAMP DEFAULT NOW() ,
    `updatetime` TIMESTAMP DEFAULT NOW() ON UPDATE NOW(),
-   `url` VARCHAR(256) NOT NULL,
+   `url` VARCHAR(255) NOT NULL,
    `postdate` VARCHAR(40) NOT NULL,
    `bank` VARCHAR(40) NOT NULL,
-   `title` VARCHAR(256) NOT NULL,
-   `notes` VARCHAR(256) NOT NULL,
+   `title` VARCHAR(255) NOT NULL,
+   `notes` VARCHAR(255) NOT NULL,
    `status` int(8) NOT NULL,
    PRIMARY KEY (id),
    UNIQUE KEY unique_url (url)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
 
